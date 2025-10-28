fd27cefab229bd6f7de593fa068f04c4cb7f784c
시원 <sw622@naver.com>
2025-10-28 16:47:03 +0900

    💄 style: 메인 페이지 UI 제작

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,34 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+  const menus = [
+    { link: "./bread-pedia", imgPath: "/images/main/breadpedia-icon.png", title: "빵 도감", description: "모든 빵을 한눈에" },
+    { link: "./category-quiz", imgPath: "/images/main/categoryquiz-icon.png", title: "카테고리 퀴즈", description: "게임으로 빵 마스터하기" },
+    { link: "#", imgPath: "/images/main/sendfeedback-icon.png", title: "피드백 보내기", description: "의견을 들려주세요" },
+  ];
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-jua dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-[url('/images/main/main-bg.jpg')] bg-white/70 bg-blend-overlay text-t-secondary">
+        <h1 className="mb-4 text-6xl text-t-primary">빵.zip</h1>
+        <p className="text-3xl">오늘도 빵빵하게 🍞</p>
+
+        <nav className="flex flex-col justify-center gap-4 flex-1 w-full">
+          <ul className="contents">
+            {menus.map((item, idx) => (
+              <li key={idx} className="block rounded-xl border border-accentgold bg-offwhite">
+                <Link href={item.link} className="flex gap-4 items-center p-4">
+                  <Image src={item.imgPath} width={70} height={70} alt={`${item.title} 아이콘`} className="h-15 object-contain" />
+                  <span className="flex flex-col">
+                    <span className="text-xl text-t-primary">{item.title}</span>
+                    <span>{item.description}</span>
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </main>
     </div>
   );
